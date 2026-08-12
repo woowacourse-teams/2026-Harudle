204f5b17a1f9fbdcec755096a8d878993050948e
jebiyeon02 <jhje5595@naver.com>
2026-08-12 23:54:45 +0900

    style(FE): 일기 작성 페이지 UI 구현

--- frontend/web/src/shared/PageHeader.tsx
+++ frontend/web/src/shared/PageHeader.tsx
@@ -1,4 +1,6 @@
 import type { ReactNode } from 'react';
+import { css } from '@emotion/react';
+import { theme } from '../styles/theme';
 
 const PageHeader = ({
   leftButton,
@@ -10,10 +12,38 @@ const PageHeader = ({
   rightButton: ReactNode | null;
 }) => {
   return (
-    <div>
-      {leftButton} {title} {rightButton}
-    </div>
+    <header css={pageHeaderStyle}>
+      <div css={buttonSlotStyle}>{leftButton}</div>
+      <h1 css={titleStyle}>{title}</h1>
+      <div css={buttonSlotStyle}>{rightButton}</div>
+    </header>
   );
 };
 
 export default PageHeader;
+
+const pageHeaderStyle = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 56px;
+  padding: 0 8px;
+  box-sizing: border-box;
+`;
+
+const buttonSlotStyle = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 32px;
+  height: 32px;
+`;
+
+const titleStyle = css`
+  color: ${theme.colors.textPrimary};
+  font-size: 22px;
+  font-weight: 700;
+  line-height: 32px;
+  text-align: center;
+`;
