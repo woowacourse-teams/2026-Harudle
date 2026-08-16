9cae431db3c6982470e154aaaa27323f289b1f6f
jebiyeon02 <jhje5595@naver.com>
2026-08-16 18:20:28 +0900

    refactor(FE): 홈 화면 색상 토큰 추가

--- frontend/web/src/styles/theme.ts
+++ frontend/web/src/styles/theme.ts
@@ -3,5 +3,14 @@ export const theme = {
     bg: {
       brand: '#8B6FE8',
     },
+    text: {
+      brand: '#7355DA',
+      primary: '#111118',
+      secondary: '#6F6B79',
+      danger: '#F15B5B',
+    },
+    border: {
+      primary: '#E8E6EB',
+    },
   },
 } as const;
