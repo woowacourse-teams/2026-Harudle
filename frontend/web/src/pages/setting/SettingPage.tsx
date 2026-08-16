6b83266e7217573c4515d8cf38e562a0212424aa
jebiyeon02 <jhje5595@naver.com>
2026-08-16 18:22:05 +0900

    feat(FE): 설정 페이지 공통 레이아웃 적용

--- frontend/web/src/pages/setting/SettingPage.tsx
+++ frontend/web/src/pages/setting/SettingPage.tsx
@@ -1,22 +1,34 @@
+import { css } from '@emotion/react';
 import BottomNavigation from '../../shared/BottomNavigation';
+import harudleLogo from '../../assets/images/harudle-logo.png';
+import { useNavigate } from 'react-router';
 
 const SettingPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <div>설정</div>
-      <div>
-        <div>계정</div>
-        <div>
-          <span>이름</span>
-          <span>정이현</span>
-        </div>
-        <div>
-          <span>소셜 계정</span>
-          <span>Kakao</span>
-        </div>
-      </div>
+    <div css={pageStyle}>
+      <header css={pageHeaderStyle}>
+        <button css={logoButtonStyle} onClick={() => navigate('/')}>
+          <img css={logoStyle} src={harudleLogo} alt="하루들" />
+        </button>
+      </header>
 
-      <button>로그아웃</button>
+      <main css={contentStyle}>
+        <div>설정</div>
+        <div>
+          <div>계정</div>
+          <div>
+            <span>이름</span>
+            <span>정이현</span>
+          </div>
+          <div>
+            <span>소셜 계정</span>
+            <span>Kakao</span>
+          </div>
+        </div>
+
+        <button>로그아웃</button>
+      </main>
 
       <BottomNavigation />
     </div>
@@ -24,3 +36,37 @@ const SettingPage = () => {
 };
 
 export default SettingPage;
+
+const pageStyle = css`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+const pageHeaderStyle = css`
+  width: 100%;
+  height: 71px;
+  box-sizing: border-box;
+`;
+
+const logoButtonStyle = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  border: none;
+  background: none;
+  cursor: pointer;
+`;
+
+const logoStyle = css`
+  width: 106px;
+  height: 71px;
+`;
+
+const contentStyle = css`
+  flex: 1;
+  overflow-y: auto;
+  padding: 20px;
+`;
