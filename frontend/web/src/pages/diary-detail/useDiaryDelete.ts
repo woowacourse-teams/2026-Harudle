598078e97e92d773dae947b81c98b81e423ef642
jebiyeon02 <jhje5595@naver.com>
2026-08-17 19:05:59 +0900

    feat(FE): 일기 상세 조회와 삭제 요청 흐름 구현

---
+++ frontend/web/src/pages/diary-detail/useDiaryDelete.ts
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import {
+  API_BASE_URL,
+  isProblemDetails,
+  RequestError,
+  type ApiRequest,
+} from '../../shared/api';
+import { useNavigate } from 'react-router';
+
+const useDiaryDelete = ({ diaryId }: { diaryId: string | undefined }) => {
+  const [diaryDeleteRequest, setDiaryDeleteRequest] = useState<
+    ApiRequest<void>
+  >({
+    status: 'idle',
+  });
+  const navigate = useNavigate();
+
+  const handleDiaryDelete = async (): Promise<void> => {
+    setDiaryDeleteRequest({ status: 'loading' });
+
+    try {
+      const response = await fetch(`${API_BASE_URL}/diaries/${diaryId}`, {
+        method: 'DELETE',
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        if (isProblemDetails(errorData)) {
+          throw new RequestError(errorData);
+        }
+
+        throw new Error('알 수 없는 에러가 발생했습니다.');
+      }
+
+      setDiaryDeleteRequest({ status: 'success', data: undefined });
+      navigate('/');
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setDiaryDeleteRequest({
+          status: 'error',
+          error: error,
+        });
+      }
+    }
+  };
+
+  return { diaryDeleteRequest, handleDiaryDelete };
+};
+
+export default useDiaryDelete;
