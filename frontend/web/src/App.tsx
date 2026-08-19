37d4cb8a9292847bdb73bdf15b3c772de24cf0f2
jebiyeon02 <jhje5595@naver.com>
2026-08-19 16:58:23 +0900

    chore(FE): 로그인 리다이렉트 임시 페이지 삭제

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -8,7 +8,6 @@ import SettingPage from './pages/setting/SettingPage';
 import LoginPage from './pages/login/LoginPage';
 import DiaryDetailPage from './pages/diary-detail/DiaryDetailPage';
 import DiarySharePage from './pages/diary-share/DiarySharePage';
-import MockKakaoLoginPage from './pages/login/MockKakaoLoginPage';
 import AuthCallbackPage from './pages/login/AuthCallbackPage';
 
 const App = () => {
@@ -16,11 +15,6 @@ const App = () => {
     <div css={appStyle}>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
-        {/* 이거 나중에 지워야됨 임시임 */}
-        <Route
-          path="/oauth2/authorization/kakao"
-          element={<MockKakaoLoginPage />}
-        />
         <Route path="/auth/callback" element={<AuthCallbackPage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/diary-write" element={<DiaryWritePage />} />
